7794a0ef137040334fdfb5c7e1611b17a45d63f7
Timi Rautamäki <timi.rautamaki@gmail.com>
2022-01-30 08:27:29 +0000

    sm7350-common: Drop Snap
    It's not included in 19.1
    
    Change-Id: Ie3f908234cf7a3d94d33ad0e9d170d3738bb9752

--- common.mk
+++ common.mk
@@ -139,7 +139,6 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
