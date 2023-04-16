300485196866f1ea541615bd7bb1fff16edcb670
Arian <arian.kulmer@web.de>
2023-04-16 17:32:17 +0200

    sm7350-common: Move to common sensors multihal
    Change-Id: Iba4df5ebb5d653cb2d3f68fce9e682d946d20935

--- common.mk
+++ common.mk
@@ -430,7 +430,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@2.1-service.xiaomi_sm8350-multihal \
+    android.hardware.sensors@2.1-service.xiaomi-multihal \
     vendor.qti.hardware.display.mapper@1.1.vendor
 
 PRODUCT_COPY_FILES += \
